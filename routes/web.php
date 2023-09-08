c9df36a97ac2e68a76442ea4755e01ce2e78c39b
jpcasanuevab <jpcasanuevab@outlook.com>
2023-09-08 10:46:51 -0300

    lavarel8_blog

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\File;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    
+    $posts = Post::all();
+    
+    return view('posts', ['posts' => $posts]);
+});
+
+Route::get('posts/{post:slug}', function(Post $post) {
+    
+    return view('post', [
+        'post' =>  $post
+    ]);
+    
+});
